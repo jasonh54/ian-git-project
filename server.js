c44c149c55a8d6240edb2ecd44a7d20c476dbbab
ianklin <ianklin18@gmail.com>
2022-11-17 19:14:18 -0800

    added about page

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get("/",(req,res)=>{
-    res.send("Hello!")
+    res.sendfile("index.html");
+})
+
+app.get("about",(req,res)=>{
+    res.sendfile("about.html");
 })
 
 app.listen(5000)
