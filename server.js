8eabd5c7b9e2bdc2095bbde9ab154d245d4b2fd1
Jason Huang <jahuang13@gmail.com>
2022-11-17 19:19:01 -0800

    added in post request

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express')
+const bodyparser = require('body-parser')
 const app = express()
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({urlencoded:true}))
 
 app.get("/",(req,res)=>{
     res.send("Hello!")
+})
+
+app.post("/postMessage",(req,res)=>{
+    console.log(req.body)
+    res.send("got the new changes")
 })
 
 app.listen(5000)
